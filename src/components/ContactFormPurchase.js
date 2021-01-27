62887636ebdcd279db48a070b99eedaa5ac4d768
eacastel <emilio@aliigo.com>
2021-01-27 10:10:54 -0800

    first upload

--- src/components/ContactFormPurchase.js
+++ src/components/ContactFormPurchase.js
@@ -39,67 +39,74 @@ export default () => {
 
   const showThankYou = (
     <div className="msg-confirm">
-      <p>Awesome! Your message was sent.</p>
+      <p>Thank you for your Inquiry! Your message was sent and you will be hearing from our staff shortly.</p>
       <button type="button" onClick={() => setSubmitted(false)}>
-        Send another message?
+        Clear Form
       </button>
     </div>
   );
 
   const showForm = (
+    <div className="">
     <form onSubmit={handleSubmit(onSubmit)} method="post">
+      <div className="mt-4">
       <label htmlFor="name">
-        <h5>Name</h5>
+        <h5 className="text-gray-700">First Name</h5>
         <input
           type="text"
           name="name"
           id="name"
           placeholder="Your name"
           ref={register}
+          className="mt-1 p-2 block w-full border-gray-400 rounded border-2"
         />
         {errors.name && <div className="msg-error">{errors.name.message}</div>}
       </label>
-
+      </div>
+      <div className="mt-4">
       <label htmlFor="email">
-        <h5>Email</h5>
+        <h5 className="text-gray-700">Email</h5>
         <input
           type="email"
           name="email"
           id="email"
           placeholder="your@email.address"
           ref={register}
+          className="mt-1 p-2 block w-full border-gray-400 rounded border-2"
         />
         {errors.email && (
           <div className="msg-error">{errors.email.message}</div>
         )}
       </label>
-
+      </div>
+      <div className="mt-4">
       <label htmlFor="question">
-        <h5>Message</h5>
+        <h5 className="text-gray-700">Message</h5>
         <textarea
           ref={register}
           name="question"
           id="question"
           rows="3"
           placeholder="Your message"
+          className="mt-1 p-2 block w-full border-gray-400 rounded border-2"
         />
         {errors.question && (
           <div className="msg-error">{errors.question.message}</div>
         )}
       </label>
-
-      <div className="submit-wrapper">
-        <button type="submit">
+          </div>
+      <div className="submit-wrapper py-4">
+        <button type="submit" className="btn-outline-primary transition duration-300 ease-in-out focus:shadow-outline border-2 border-blue-800 hover:bg-blue-800 text-blue-800 hover:text-white font-bold py-2 px-4 rounded">
           Send
         </button>
       </div>
     </form>
+    </div>
   );
 
   return (
     <div className="page contact-page">
       <div className="text-side">
-        <h2>Contact me</h2>
         {errors && errors.submit && showSubmitError(errors.submit.message)}
       </div>
       <div className="form-side">{submitted ? showThankYou : showForm}</div>
--- src/pages/repairs.js
+++ src/pages/repairs.js
@@ -1,31 +1,35 @@
 import React from 'react';
 import Layout from '../components/layout';
-import Head from '../components/head'
+import Head from '../components/head';
+import ContactFormRepairs from "../components/ContactFormRepairs";
 
 const RepairsPage = () => {
   return (
-    <div className="px-6">
+    <div>
       <Layout>
       <Head title="Repairs" />
-      <h1 className="py-4 text-2xl font-bold  text-gray-900 leading-tight">Repairs</h1>
+        <div className="px-6">
+        <h1 className="py-4 text-2xl font-bold  text-gray-900 leading-tight">Equipment Repair</h1>
 
-        <p>
-          We repair all makes and models of flexible endoscopes in our state of
-          the art service center.
-        </p>
-        <div>
-          Use the form below to schedule a repair.
-        </div>
-        <p>
-          Our technicians are professionally trained and have years of
-          experience executing both major and minor repairs. All equipment is
-          calibrated and tested to meet the manufacturer's specifications. We
-          offer a full range of repair services.
-        </p>
-
-
-      
+          <p>
+            We repair all makes and models of flexible endoscopes in our state of
+            the art service center.
+          </p>
+          <p>
+            Our technicians are professionally trained and have years of
+            experience executing both major and minor repairs. All equipment is
+            calibrated and tested to meet the manufacturer's specifications. We
+            offer a full range of repair services.
+          </p>
+            <div className="p-8">
+              <h5 class="text-lg font-bold">Use our Equipment Repair Order Form below to order a repair:</h5>
+              <ContactFormRepairs />
+            </div>
+            <p>NOTE: Cosmetic issues, i.e., scratches, dents, discolorations, etc., will not be recommended for repair unless those issue are causing an equipment system
+failure. If you would like cosmetic issues to be repaired, please note these in your description of the problem.</p>
+          </div>
       </Layout>
+
     </div>
   );
 };
