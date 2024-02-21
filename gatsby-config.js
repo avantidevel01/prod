b0428ccdfba2c28cb605cc461c404059f2786814
eacastel <emilio@aliigo.com>
2024-02-21 05:45:11 -0800

    post-mortem-nono

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,51 +3,5 @@ require("dotenv").config({
 })
 
 module.exports = {
-  siteMetadata: {
-    title: "3 West Medical",
-    author: "3 West Medical",
-    siteUrl: "https://3westmedical.com",
-    keywords:
-      "Endoscpe Repairs, Rigid Endoscopes, Flexible Endoscopes, Endoscopy Equipment",
-    description:
-      "Specializing in Endoscope Repair and Refurbished Endoscope Sales in the Area of Los Angeles.",
-  },
-  plugins: [
-    `gatsby-plugin-postcss`,
-    `gatsby-plugin-netlify`,
-    {
-      resolve: "gatsby-plugin-google-analytics",
-      options: {
-        trackingId: `${process.env.G_ANALYTICS}`,
-      },
-    },
-    {
-      resolve: `gatsby-plugin-robots-txt`,
-      options: {
-        host: "https://3westmedical.com",
-        sitemap: "https://3westmedical.com/sitemap.xml",
-        policy: [{ userAgent: "*", allow: "/" }],
-      },
-    },
-    `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-sitemap`,
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: "images",
-        path: "./src/images/",
-      },
-      __key: "images",
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: "pages",
-        path: "./src/pages/",
-      },
-      __key: "pages",
-    },
-  ],
+  
 }
